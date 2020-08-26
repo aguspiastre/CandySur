425ddeb55e67420d3f70abf99747f8a405192252
Agustin Piastrellini <agustin.piastrellini@hotmail.com>
2020-08-25 22:32:15 -0300

    wqqeqwe

---
+++ CandySur.UI/Backup-Restore/BackupRestore.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace CandySur.UI.Backup_Restore
+{
+    partial class BackupRestore
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.btnNuevoBackup = new System.Windows.Forms.Button();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.cmbBackup = new System.Windows.Forms.ComboBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRestore = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnNuevoBackup);
+            this.groupBox1.Controls.Add(this.groupBox2);
+            this.groupBox1.Controls.Add(this.pictureBox1);
+            this.groupBox1.Controls.Add(this.btnRestore);
+            this.groupBox1.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(3, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(602, 239);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "BACK-UP Y RESTORE";
+            // 
+            // btnNuevoBackup
+            // 
+            this.btnNuevoBackup.BackColor = System.Drawing.Color.Transparent;
+            this.btnNuevoBackup.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNuevoBackup.ForeColor = System.Drawing.Color.Blue;
+            this.btnNuevoBackup.Location = new System.Drawing.Point(350, 199);
+            this.btnNuevoBackup.Name = "btnNuevoBackup";
+            this.btnNuevoBackup.Size = new System.Drawing.Size(118, 31);
+            this.btnNuevoBackup.TabIndex = 18;
+            this.btnNuevoBackup.Text = "Nuevo Back-up";
+            this.btnNuevoBackup.UseVisualStyleBackColor = false;
+            this.btnNuevoBackup.Click += new System.EventHandler(this.btnNuevoBackup_Click);
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.cmbBackup);
+            this.groupBox2.Location = new System.Drawing.Point(9, 98);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(583, 95);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "BACK-UP DISPONIBLES";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 22);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(64, 19);
+            this.label5.TabIndex = 18;
+            this.label5.Text = "Back-up:";
+            // 
+            // cmbBackup
+            // 
+            this.cmbBackup.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbBackup.FormattingEnabled = true;
+            this.cmbBackup.Location = new System.Drawing.Point(10, 44);
+            this.cmbBackup.Name = "cmbBackup";
+            this.cmbBackup.Size = new System.Drawing.Size(263, 26);
+            this.cmbBackup.TabIndex = 29;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::CandySur.UI.Properties.Resources.FINAL;
+            this.pictureBox1.Location = new System.Drawing.Point(10, 20);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(582, 72);
+            this.pictureBox1.TabIndex = 5;
+            this.pictureBox1.TabStop = false;
+            // 
+            // btnRestore
+            // 
+            this.btnRestore.BackColor = System.Drawing.Color.Transparent;
+            this.btnRestore.Font = new System.Drawing.Font("Calibri", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRestore.ForeColor = System.Drawing.Color.Green;
+            this.btnRestore.Location = new System.Drawing.Point(474, 199);
+            this.btnRestore.Name = "btnRestore";
+            this.btnRestore.Size = new System.Drawing.Size(118, 31);
+            this.btnRestore.TabIndex = 2;
+            this.btnRestore.Text = "Realizar Restore";
+            this.btnRestore.UseVisualStyleBackColor = false;
+            this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+            // 
+            // BackupRestore
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(611, 251);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "BackupRestore";
+            this.Text = "Backup y Restore";
+            this.Load += new System.EventHandler(this.BackupRestore_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox cmbBackup;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRestore;
+        private System.Windows.Forms.Button btnNuevoBackup;
+    }
+}
---
+++ CandySur.UI/Backup-Restore/BackupRestore.cs
@@ -0,0 +1,87 @@
+﻿using CandySur.SEG.Util;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static CandySur.SEG.Util.Enums;
+
+namespace CandySur.UI.Backup_Restore
+{
+    public partial class BackupRestore : Form
+    {
+        private CandySur.SEG.Entity.SessionManager Session;
+        private const string RUTA_DESTINO = "C:\\Program Files (x86)\\Microsoft SQL Server\\MSSQL.1\\MSSQL\\Backup";
+        SEG.Service.DataBase databaseService = new SEG.Service.DataBase();
+        SEG.Service.Bitacora bitacoraService = new SEG.Service.Bitacora();
+
+        public BackupRestore()
+        {
+            InitializeComponent();
+        }
+
+        private void BackupRestore_Load(object sender, EventArgs e)
+        {
+            Session = SEG.Entity.SessionManager.GetInstance();
+
+            DirectoryInfo d = new DirectoryInfo(@"C:\\Program Files (x86)\\Microsoft SQL Server\\MSSQL.1\\MSSQL\\Backup");
+            FileInfo[] Files = d.GetFiles("*CandySur*");
+
+            foreach (FileInfo file in Files) { cmbBackup.Items.Add(file); }
+        }
+
+        private void btnRestore_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                databaseService.RealizarRestore("CandySur", cmbBackup.SelectedItem.ToString());
+
+                SEG.Entity.Bitacora reg = new SEG.Entity.Bitacora
+                {
+                    IdUsuario = Session.Usuario.Id,
+                    IdCriticidad = (int)Enums.Criticidad.Alta,
+                    Fecha = DateTime.Now,
+                    Descripcion = "Restore realizado con exito"
+                };
+
+                bitacoraService.Registrar(reg);
+
+                MessageBox.Show("Se ha realizado el restore de la base de datos de forma correcta", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void btnNuevoBackup_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string fullUri = RUTA_DESTINO + "\\CandySur" + "_" + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss");
+                databaseService.NuevoBackup("CandySur", fullUri);
+
+                SEG.Entity.Bitacora reg = new SEG.Entity.Bitacora
+                {
+                    IdUsuario = 1,
+                    IdCriticidad = (int)Enums.Criticidad.Alta,
+                    Fecha = DateTime.Now,
+                    Descripcion = "Backup guardado con exito"
+                };
+
+                bitacoraService.Registrar(reg);
+
+                MessageBox.Show("Se ha guardo el backup de la base de datos de forma correcta", "OK", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message.ToString(), "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+    }
+}
